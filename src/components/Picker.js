34561f966cedd1d8d3120e05ff04de26dc574afa
Donghee Lee <jacobdhlee@gmail.com>
2019-04-03 14:29:14 -0400

    change picker css and add image for readme`

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components";
 import PickersSelect from "react-native-picker-select";
 
@@ -7,12 +7,31 @@ const PickerContainer = styled.View`
   width: 300;
   height: 50;
   margin: 5px;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #80a9ed;
+  border-radius: 20;
 `;
+
+const pickerStyle = {
+  inputIOS: {
+    color: "grey",
+    fontSize: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center"
+  },
+  inputAndroid: {
+    color: "white"
+  },
+  underline: { borderTopWidth: 0 }
+};
 
 const Pickers = ({ onValueChange, value, items, onDonePress }) => {
   return (
     <PickerContainer>
       <PickersSelect
+        style={pickerStyle}
         onValueChange={onValueChange}
         value={value}
         items={items}
